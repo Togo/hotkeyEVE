f5e4c8d888e923282244cd9c58e7754eadc20101
Tobias Sommer <togo210@googlemail.com>
2012-11-04 09:16:57 +0100

    log

--- hotkeyEVE/Conroller/UIElementClickedController.m
+++ hotkeyEVE/Conroller/UIElementClickedController.m
@@ -15,6 +15,7 @@
   DDLogInfo(@"Received Click on UI Element: %@", [[element owner] appName]);
   DDLogInfo(@"Role: %@", [element role]);
   DDLogInfo(@"Role Description: %@", [element roleDescription]);
+  DDLogInfo(@"Identifier: %@", [element uiElementIdentifier]);
   // Check App Status
   // Check App Active
   // Check App GUI Element Active
