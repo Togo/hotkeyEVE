720ff17516fd57cd230383db258fcbfbf4159507
Tobias Sommer <togo210@googlemail.com>
2012-11-24 18:18:08 +0100

    gui elements iCal es

--- hotkeyEVE/Database/SQL Scripte/V1.3.2/INSERT_GUI_SUPPORT_V1_3_2.sql
+++ hotkeyEVE/Database/SQL Scripte/V1.3.2/INSERT_GUI_SUPPORT_V1_3_2.sql
@@ -1,3 +1,4 @@
 INSERT INTO "gui_supported_apps" VALUES (11, 'Finder', 'com.apple.finder', 'es');
 INSERT INTO "gui_supported_apps" VALUES (12, 'Chrome', 'com.google.Chrome', 'es');
 INSERT INTO "gui_supported_apps" VALUES (13, 'Safari', 'com.apple.Safari', 'es');
+INSERT INTO "gui_supported_apps" VALUES (14, 'Calendar', 'com.apple.iCal', 'es');
